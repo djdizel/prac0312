7aea357b71dcfab02236504de7b8b00bea90320c
sap1ks <sap1kcsgo@gmail.com>
2024-12-03 10:55:00 +0200

    111

--- Program.cs
+++ Program.cs
@@ -10,41 +10,6 @@ namespace ConsoleApp2
 /// </summary>
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            int size = Size();
-            int[] omas = Input(size);
-            Output(omas);
-            Console.Read();
-        }
-        static int Size()
-        {
-            Console.Write("Введите размер = ");
-            int size;
-            while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
-            {
-                Console.WriteLine("Ошибка!");
-            }
-            return size;
-        }
-        static int[] Input(int size)
-        {
-            int[] omas = new int[size]; 
-            Console.WriteLine("Заполнение одномерного массива");
-            for (int i = 0; i < size; i++)
-            {
-                Console.Write($"omas[{i}] =");
-                omas[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            return omas;
-        }
-        static void Output(int[] omas)
-        {
-            Console.WriteLine("Вывод одномерного масива");
-            for (int i = 0; i < omas.Length; i++)
-            {
-                Console.WriteLine($"omas[{i}] = {omas[i]}");
-            }
-        }
+        
     }
 }
