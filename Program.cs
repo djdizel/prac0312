812e344b0cea85ff96c22d33f7f41ae5a9aadd33
sap1ks <sap1kcsgo@gmail.com>
2024-12-03 11:22:04 +0200

    qweqwe

--- Program.cs
+++ Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+        namespace ConsoleApp2
 {/// <summary>
-/// Заполнить одномерный массив и вывести в консоль
+/// Массив предназначен для хранения роста 12 чел с помощью rng заполнить массив целыми значениями 
+/// лежащими в диапозоне 163-190 вывести значение элементов циклом foreach,найти ср знач роста 
 /// </summary>
     internal class Program
     {
-        
+        static void Main(string[] args)
+        {
+            int[] omas = Input();
+            int v = Avg(omas);
+            Output($"Среднее значение{v}");
+            Console.Read();
+        }
+        static int[] Input()
+        {
+            int a = 0;
+            int[] omas = new int[12];
+            Random rng = new Random();
+            Console.WriteLine("Вывод одномерного масива");
+            foreach (int i in omas)
+            {
+                omas[i] = rng.Next(163, 190);
+                Console.WriteLine($"omas[{a++}] = {omas[i]}");
+            }
+            return omas;
+        }
+        static int Avg(int[] omas)
+        {
+            int sum = 0;
+            foreach (int i in omas)
+            {
+                sum += i;
+                Console.WriteLine(sum);
+            }
+            return sum / omas.Length;
+        }
+        static void Output(string message)
+        {
+            Console.WriteLine(message); 
+        }
     }
 }
